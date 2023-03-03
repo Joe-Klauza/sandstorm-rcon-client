3349be86b546b34afb54100533a5b1ecd1d8ed8b
Joe Klauza <joeklauza@gmail.com>
2023-03-02 22:46:41 -0500

    Update build.sh to exclude existing shasum file from shasums

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ set -x
 go mod tidy
 env GOOS=linux   GOARCH=amd64 go build -ldflags "-s -w"
 env GOOS=windows GOARCH=amd64 go build -ldflags "-s -w"
+sha512sum sandstorm-rcon-client{,.exe} > sandstorm-rcon-client-sha512sums.txt
 ls -alh sandstorm-rcon-client*
-sha512sum sandstorm-rcon-client* > sandstorm-rcon-client-sha512sums.txt
 set +x
 
 popd || exit 1
